23f27840ac37343818745991234843d281ce7ef5
Geralt | Web3 <cryptonsorav@gmail.com>
2026-01-01 16:12:26 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
-import Navbar from "../components/PictoNavbar";
-import Hero from "../components/PictoHero";
-import Services from "../components/PictoServices";
-import Projects from "../components/Projects";
-import Contact from "../components/PictoContact";
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import WhatIDo from '../components/WhatIDo';
+import Skills from '../components/Skills';
+import TechStack from '../components/TechStack';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Services />
+      <WhatIDo />
+      <Skills />
+      <TechStack />
       <Projects />
       <Contact />
     </>
